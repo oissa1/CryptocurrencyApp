4ba81c1757faebd3bd2b19f54ce58676dfaf0cfa
Omar Issa <oiattar1@gmail.com>
2024-04-07 02:23:58 -0700

    finished app

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/presentation/coin_list/components/CoinListItem.kt
@@ -0,0 +1,57 @@
+package com.plcoding.cryptocurrencyappyt.presentation.coin_list.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+
+@Composable
+fun CoinListItem(
+    coin: Coin,
+    onItemClick: (Coin) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick(coin) }
+            .padding(20.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        with(coin) {
+            Text(
+                text = "$rank. $name ($symbol)",
+                style = MaterialTheme.typography.body1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = if (isActive) "active" else "inactive",
+                color = if (isActive) Color.Green else Color.Red,
+                fontStyle = FontStyle.Italic,
+                textAlign = TextAlign.End,
+                style = MaterialTheme.typography.body2,
+                modifier = Modifier.align(CenterVertically)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun CoinListItemPreview() {
+    CoinListItem(coin = Coin("1", true, "Bitcoin", 1, "BTC")) {
+
+    }
+}
